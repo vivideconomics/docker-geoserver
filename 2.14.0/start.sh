9e861e9e7ca1a8f49feb8ffdf30afcba39b7f0d8
Mark Westcott <mark.westcott@vivideconomics.com>
2018-11-22 15:59:36 +0000

    Use CATALINA_OPTS for INITIAL_HEAP_SIZE and MAXIMUM_HEAP_SIZE

--- 2.14.0/start.sh
+++ 2.14.0/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+set -x
+
 for ext in `ls -d /var/local/geoserver-exts/*/`; do
   cp "${ext}"*.jar /usr/local/geoserver/WEB-INF/lib
 done
-
+CATALINA_OPTS="$CATALINA_OPTS -Xms${INITIAL_HEAP_SIZE:-256M} -Xmx${MAXIMUM_HEAP_SIZE:-512M}"
 catalina.sh run
-
